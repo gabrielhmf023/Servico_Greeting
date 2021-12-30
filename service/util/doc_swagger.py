2ca53ab88f67096b82a53fff2beccb69061ab76a
Gabriel Henrique <gabrielhmf023@gmail.com>
2021-12-30 09:38:34 -0300

    Atualizando o arquivo doc_swagger.py

--- service/util/doc_swagger.py
+++ service/util/doc_swagger.py
@@ -3,4 +3,4 @@ from service.restplus import api
 
 INPUT_MAIN_SERVICE = api.model(
   'input_main_service', {
-    'textoMensagem': fields.List(fields.String(), required=True, description="Nome que o user ofereceu")})
+    'textoMensagem': fields.List(fields.String(), required=True, description="Nome que o usuario forneceu")})
